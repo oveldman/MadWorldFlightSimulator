aa1017829e06be4e54d8b5dfbf8d22f304d1921f
Oscar Veldman <oveldman@gmail.com>
2023-10-03 18:13:10 +0200

    Remove Android

--- FlightSimulator/MadWorld.FlightSimulator.IOS.Infrastructure/Platforms/Android/PlatformClass1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MadWorld.FlightSimulator.IOS.Infrastructure
-{
-    // All the code in this file is only included on Android.
-    public class PlatformClass1
-    {
-    }
-}
--- FlightSimulator/MadWorld.FlightSimulator.IOS.Infrastructure/Platforms/Tizen/PlatformClass1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace MadWorld.FlightSimulator.IOS.Infrastructure
-{
-    // All the code in this file is only included on Tizen.
-    public class PlatformClass1
-    {
-    }
-}
